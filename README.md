1d5fa2d7ec0411220e043a1f59eb8b5adb868e8f
e <1456457523@qq.com>
2020-02-23 23:07:49 +0800

    add function digital_write

--- README.md
+++ README.md
@@ -4,32 +4,53 @@
 * 初始化  把PLC使用的串口号传入就能完成初始化  
     `fx_plc = FX_PLC_CTR('com5')`
 * 读取PLC内部参数  
-  `fx_plc.digitalRead(Regional,point,read_byte_number=1,_bit=1,raw=0)`  
-
-  * Regional为PLC内部存储区域，暂时支持X，Y，D，M  
+  `fx_plc.digital_read(Regional,point,read_byte_number=1,_bit=1,raw=0)`  
+  * Regional为PLC内部存储区域，暂时支持X，Y，D，M 不支持D区特殊继电器，如D8000以后的）  
   * point 为PLC地址编号  
   * read_byte_number 为一次读取的BYTE数，默认为1，读取8位数据，在Regional为D时失效  
   * _bit 为只输出当前地址的数据，默认为1，0/1可选，当该值为1时且read_byte_number=1，输出当前地址的值，在Regional为d时失效 
   * raw 为输出PLC返回的原始值的开关，默认为0。当为1时，该函数返回PLC输出的原始数据  
+* 写入PLC参数  
+  `digital_write(Regional,point,data,_bit=1,write_byte_number=1)`  
+  * Regional为PLC内部存储区域，暂时支持Y，D，M（不支持D区特殊继电器，如D8000以后的） 
+  * point 为PLC地址编号
+  * data 为写入值，在对Y区和M区，值为1，0（通/断），在D区为二进制字符串
+  * _bit 为只修改当前bit，默认为1，为0时，data中需写入整个byte的数据。该参数对D区无效
+  * write_byte_number 当目标区域为Y或M区且_bit=时无效。该参数为一次写入的byte量，推荐不能超过4  
+  * 该函数的返回值为1，表示执行成功，为0表示执行失败
 ## 栗子
   * 读取X0的值
-    `fx_plc.digitalRead('x',0)`  
+    `fx_plc.digital_read('x',0)`  
     返回值为0，表示当前X0无输入；返回1，表示当前X0有输入  
 
   * 读取Y0-Y7的值
-    `fx_plc.digitalRead('y',0,_bit=0)`  
+    `fx_plc.digital_read('y',0,_bit=0)`  
     point为0-7中的任意值即可，返回值为00000010，表示Y7,Y6,Y5,Y4,Y3,Y2,Y1,Y0的状态，由此可知，Y1处于接通状态  
 
   * 读取Y0-Y17的值
-    `fx_plc.digitalRead('y',0,2)`   
+    `fx_plc.digital_read('y',0,2)`   
     返回值为 0000000000000010，对应Y17-Y0的状态  
 
   * 读取M8-M15的PLC原始返回值
-    `fx_plc.digitalRead('m',8,raw=1)`  
+    `fx_plc.digital_read('m',8,raw=1)`  
     返回值为 2 30 30 3 36 33，可对照三菱PLC通信协议自行解读  
 
   * 读取D123的值 
-    `fx_plc.digitalRead('d',123)`  
+    `fx_plc.digital_read('d',123)`  
     返回值为0000000000000000，一个D占用16bit的空间
+  
+  * 修改M128的值为1 
+    `fx_plc.digital_write('m',128,1)`  
+    返回值为1，表示操作成功  
+
+  * 修改Y7-Y0的值为'00110101'
+    `fx_plc.digital_write('y',0,'00110101',0)`  
+    返回值为1，表示操作成功 其中point可为Y0-Y7中任意值  
+
+  * 修改D509的值为10 `fx_plc.digital_write('d',509,fx_plc.dec_2_bin(10))`  
+    由于data参数需要二进制值，在此使用内部提供的方法`dec_2_bin(10)` 把十进制值10转换成二进制字符串    
+
+  * 修改M0-M15的值 `fx_plc.digital_write('m',0,'1000000000000001',0,2)` 
+    返回值1，表示操作成功，查看程序，M0和M15吸合
 
 
--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# import serial
-# ser = serial.Serial(
-# port='com5',
-# baudrate=9600,     # baud rate
-# bytesize=7,   # number of databits
-# parity=PARITY_EVEN,   # enable parity checking
-# stopbits=STOPBITS_ONE, # number of stopbits
-# timeout=None,      # set a timeout value, None for waiting forever
-# )
-
-# ser.send()
-
-
-
 import serial
 import string
 import binascii
@@ -82,12 +68,12 @@ class FX_PLC_CTR(object):
         b = self.Hexadecimal_2_ascii(cache[-1])
         return (a,b)
 
-    def hex_2_dec(self,num):
+    def hex_2_bin(self,num):
         num = int("0x%s" % num,16)
         num = bin(num)[2:].zfill(4)
         return num
 
-    def digitalRead(self,Regional,point,read_byte_number=1,_bit=1,raw=0):
+    def digital_read(self,Regional,point,read_byte_number=1,_bit=1,raw=0):
         # 构造元件读取请求参数
         cache = ['30']
         # 检测参数是否合法
@@ -102,20 +88,20 @@ class FX_PLC_CTR(object):
             self.logger('DEBUG','读取区块地址{}'.format(address))
             for i in address:
                 cache.append(self.Hexadecimal_2_ascii(i))
-        if Regional.upper() == 'Y':
+        elif Regional.upper() == 'Y':
             col= self.get_address_area(point,8)
             address = '00A' + str(col)
             self.logger('DEBUG','读取区块地址{}'.format(address))
             for i in address:
                 cache.append(self.Hexadecimal_2_ascii(i))
-        if Regional.upper() == 'M':
+        elif Regional.upper() == 'M':
             col= self.get_address_area(point)
             row = int((int(point) / 128))
             address = '01'+str(row) + str(col)
             self.logger('DEBUG','读取区块地址{}'.format(address))
             for i in address:
                 cache.append(self.Hexadecimal_2_ascii(i))
-        if Regional.upper() == 'D':
+        elif Regional.upper() == 'D':
             _bit=0
             read_byte_number=2
             # 计算列分布
@@ -127,12 +113,18 @@ class FX_PLC_CTR(object):
             if point < 128:
                 address = '10'+ str(col) + row 
             elif point > 127 and point <256:
-                address = '11'+ str(col) + row 
+                address = '11'+ str(col) + row
+            elif point > 255 and point <384:
+                address = '12'+ str(col) + row 
+            elif point > 383 and point <512:
+                address = '12'+ str(col) + row 
             else:
                 raise  Exception("不存在该地址")
             self.logger('DEBUG','读取区块地址{}'.format(address))
             for i in address:
                 cache.append(self.Hexadecimal_2_ascii(i))
+        else:
+            raise  Exception("不存在该区域")
  
     # 构造读取比特开始值
         cache.append('30')
@@ -169,8 +161,8 @@ class FX_PLC_CTR(object):
             payload = receive_data[1:-3]
             # 在read_byte_number=1且_bit=1时，返回单个地址的值
             if read_byte_number == 1 and _bit:
-                a = self.hex_2_dec(self.ascii_2_Hexadecimal(payload[1]))
-                b = self.hex_2_dec(self.ascii_2_Hexadecimal(payload[0]))
+                a = self.hex_2_bin(self.ascii_2_Hexadecimal(payload[1]))
+                b = self.hex_2_bin(self.ascii_2_Hexadecimal(payload[0]))
                 p = 8 - (int(point)%8)
                 c = (b+a)[p-1:p]
                 return int(c)
@@ -178,15 +170,134 @@ class FX_PLC_CTR(object):
             else:
                 cache = ''
                 for i in range(len(payload),0,-2):
-                    a = self.hex_2_dec(self.ascii_2_Hexadecimal(payload[i-1]))
-                    b = self.hex_2_dec(self.ascii_2_Hexadecimal(payload[i-2]))
+                    a = self.hex_2_bin(self.ascii_2_Hexadecimal(payload[i-1]))
+                    b = self.hex_2_bin(self.ascii_2_Hexadecimal(payload[i-2]))
                     cache += (b+a)
                 return cache
 
 
+    def digital_write(self,Regional,point,data,_bit=1,write_byte_number=1):
+        cache = ['31']
+        # 检测参数是否合法
+        if not Regional.upper() in 'YMD':
+            raise  Exception("{}区暂不支持本操作".format(Regional.upper()))
+        if _bit == 1 and Regional.upper() in 'YM':
+            write_byte_number=1
+        # 计算不同分区的地址
+        if Regional.upper() == 'Y':
+            # 获取要修改的元件地址
+            col= self.get_address_area(point,8)
+            address = '00A' + str(col)
+            self.logger('DEBUG','准备修改的区块地址{}'.format(address))
+            for i in address:
+                cache.append(self.Hexadecimal_2_ascii(i))
+            # 修改指定byte
+            cache.append('30')
+            cache.append(self.Hexadecimal_2_ascii(write_byte_number))
+            # 只修改单个bit
+            if _bit == 1:
+                # 获取位数
+                _point = point % 10
+                # 检查点数是否合法
+                if _point == 8 or _point == 9:
+                    raise  Exception("请检测是否存在该地址")
+                # 获取原始值
+                original_data = self.digital_read('Y',point,_bit=0)
+                self.logger('DEBUG','PLC原始数据为{}'.format(original_data))
+                # 计算生成数据
+                data = original_data[0:7-_point] + str(data) + original_data[8-_point:]
+                self.logger('DEBUG','计算出的结果为{}'.format(data))
+            # 修改整个byte
+            else:
+                data = str(data).zfill(8*int(write_byte_number))
+                self.logger('DEBUG','准备修改结果为{}'.format(data))
 
 
-            
+        elif Regional.upper() == 'M':
+            col= self.get_address_area(point)
+            row = int((int(point) / 128))
+            address = '01'+str(row) + str(col)
+            self.logger('DEBUG','准备修改的区块地址{}'.format(address))
+            for i in address:
+                cache.append(self.Hexadecimal_2_ascii(i))
+            # 修改指定个byte
+            cache.append('30')
+            cache.append(self.Hexadecimal_2_ascii(write_byte_number))
+            if _bit == 1:
+                # 获取位数
+                _point = point % 8
+                # 获取原始值
+                original_data = self.digital_read('M',point,_bit=0)
+                self.logger('DEBUG','PLC原始数据为{}'.format(original_data))
+                # 计算生成数据
+                data = original_data[0:7-_point] + str(data) + original_data[8-_point:]
+                self.logger('DEBUG','计算出的结果为{}'.format(data))
+            # 修改整个byte
+            else:
+                data = str(data).zfill(8*int(write_byte_number))
+                self.logger('DEBUG','准备修改结果为{}'.format(data))
+
+
+        elif Regional.upper() == 'D':
+            if write_byte_number < 2:
+                write_byte_number = 2
+            if not write_byte_number % 2 == 0:
+                write_byte_number += 1
+
+            # 计算列分布
+            row= int(point) *2
+            col = int(point / 8)
+            row = str(hex(row)[-1].upper())
+            col = str(hex(col)[-1].upper())
+            # 生成地址
+            if point < 128:
+                address = '10'+ str(col) + row 
+            elif point > 127 and point <256:
+                address = '11'+ str(col) + row 
+            elif point > 255 and point <384:
+                address = '12'+ str(col) + row 
+            elif point > 383 and point <512:
+                address = '12'+ str(col) + row 
+            else:
+                raise  Exception("不存在该地址")
+            self.logger('DEBUG','准备修改的区块地址{}'.format(address))
+            for i in address:
+                cache.append(self.Hexadecimal_2_ascii(i))
+            # 修改指定个byte
+            cache.append('30')
+            cache.append(self.Hexadecimal_2_ascii(write_byte_number))
+            data = str(data).zfill(8*int(write_byte_number))
+            self.logger('DEBUG','准备修改结果为{}'.format(data))
+        else:
+            raise  Exception("未找到该种区域")
+
+
+
+        for i in range(len(data),0,-8):
+            high = self.Hexadecimal_2_ascii(hex(int(data[i-8:i-4],2))[-1])
+            low = self.Hexadecimal_2_ascii(hex(int(data[i-4:i],2))[-1])
+            # 写入数据
+            cache.append(high)
+            cache.append(low)
+        # 结束字符
+        cache.append('03')
+        # 校验码
+        a,b = self.Checksum(cache)
+        cache.append(a)
+        cache.append(b)
+        # 构造最终输出指令
+        output = '02 '
+        for i in cache:
+            output += '%s ' % i
+        self.logger('DEBUG','准备向PLC发送指令：{}'.format(output))
+        receive_data = self.send(output)
+
+        if receive_data[0] == 6:
+            return 1
+        else:
+            return 0 
+
+
 
 
     def send(self,data):
@@ -203,10 +314,21 @@ class FX_PLC_CTR(object):
         return receive
     
 
+    def dec_2_bin(self,num):
+        payload = str(bin(int(num))[2:])
+        num_len = len(payload)
+        if not num_len % 8 == 0:
+            num_len += 8 - (num_len % 8)
+            payload = payload.zfill(num_len)
+        return payload
+    
 
 
-fx_plc = FX_PLC_CTR('com5')
-print(fx_plc.digitalRead('d',123))
+if __name__ == '__main__':
+    fx_plc = FX_PLC_CTR('com5')
+    fx_plc.digital_write('d',123,fx_plc.dec_2_bin(10))
+    print(fx_plc.digital_read('d',123))
+    
 
 
 
